092c0e7512d040763b9f61d377fcf4cd3cbce2f6
Dilum Aluthge <dilum@aluthge.com>
2020-02-25 02:41:48 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function run() {
     try {
         const inline: string = core.getInput('inline')
         // Run Pkg.test
-        await exec.exec('julia', ['--color=yes', '--check-bounds=yes', '--inline=${inline}', '--project', '-e', 'using Pkg; Pkg.test(coverage=true)'])
+        await exec.exec('julia', ['--color=yes', '--check-bounds=yes', `--inline=${inline}`, '--project', '-e', 'using Pkg; Pkg.test(coverage=true)'])
     } catch (error) {
         core.setFailed(error.message)
     }
