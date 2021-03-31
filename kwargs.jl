78c6039ef2f66a7eed1a234e61e2f63d556bf3f5
Dilum Aluthge <dilum@aluthge.com>
2021-03-30 20:10:20 -0400

    Reduce nesting

--- kwargs.jl
+++ kwargs.jl
@@ -4,24 +4,26 @@ include(joinpath(@__DIR__, "autodetect-dependabot.jl"))
 
 function kwargs(; coverage::Bool,
                   force_latest_compatible_version::Union{Bool, Symbol})
-    is_dependabot_job = AutodetectDependabot.is_dependabot_job()
+    if !(force_latest_compatible_version isa Bool) && (force_latest_compatible_version != :auto)
+        throw(ArgumentError("Invalid value for force_latest_compatible_version: $(force_latest_compatible_version)"))
+    end
+
     kwargs_dict = Dict{Symbol, Any}()
     kwargs_dict[:coverage] = coverage
-
-    if VERSION >= v"1.7.0-" # excludes 1.6, includes 1.7-DEV, includes 1.7
-        if force_latest_compatible_version isa Bool
-            kwargs_dict[:force_latest_compatible_version] = force_latest_compatible_version
-        elseif force_latest_compatible_version == :auto
-            is_dependabot_job && @info("This is a Dependabot/CompatHelper job, so `force_latest_compatible_version` has been set to `true`")
-            kwargs_dict[:force_latest_compatible_version] = is_dependabot_job
-        else
-            throw(ArgumentError("Invalid value for force_latest_compatible_version: $(force_latest_compatible_version)"))
-        end
-    else
-        if force_latest_compatible_version != :auto
-            @warn("The `force_latest_compatible_version` option requires at least Julia 1.7", VERSION, force_latest_compatible_version)
-        end
+  
+    if VERSION < v"1.7.0-"
+        (force_latest_compatible_version != :auto) && @warn("The `force_latest_compatible_version` option requires at least Julia 1.7", VERSION, force_latest_compatible_version)
+        return kwargs_dict
     end
+  
+    if force_latest_compatible_version == :auto
+        is_dependabot_job = AutodetectDependabot.is_dependabot_job()
+        is_dependabot_job && @info("This is a Dependabot/CompatHelper job, so `force_latest_compatible_version` has been set to `true`")
+        kwargs_dict[:force_latest_compatible_version] = is_dependabot_job
+    else
+        kwargs_dict[:force_latest_compatible_version] = force_latest_compatible_version::Bool
+    end
+
     return kwargs_dict
 end
 
