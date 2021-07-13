8442719035ce11cccbc15de031da098bde09c75c
Ian Butterworth <i.r.butterworth@gmail.com>
2021-07-13 15:57:43 -0400

    Update kwargs.jl
    Co-authored-by: Sascha Mann <git@mail.saschamann.eu>

--- kwargs.jl
+++ kwargs.jl
@@ -17,7 +17,7 @@ function kwargs(; coverage::Bool,
     if VERSION >= v"1.6.0"
         kwargs_dict[:julia_args] = julia_args
     elseif julia_args != ``
-        @warn("The `julia_args` option requires at least Julia 1.6", VERSION, julia_args)
+        println("::warning::The `julia_args` option requires at least Julia 1.6. VERSION=$VERSION, julia_args=$julia_args")
     end
 
     if VERSION < v"1.7.0-" || !hasmethod(Pkg.Operations.test, Tuple{Pkg.Types.Context, Vector{Pkg.Types.PackageSpec}}, (:force_latest_compatible_version,))
