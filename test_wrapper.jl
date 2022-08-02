90e6a4c313e049ea6758f15c247e70d4e526f20c
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2022-08-02 03:25:24 +0200

    wip

--- test_wrapper.jl
+++ test_wrapper.jl
@@ -1,50 +1,78 @@
 module TestWrapper
 using Pkg, GitHubActions, Logging
 
-function parse_and_relog(output)
-    r = r"(\e\[91m\e\[1m)?Test Failed(\e\[22m\e\[39m)? at (\e\[39m\e\[1m)?(?<path>[^\s\e]+)(\e\[22m)?\n\h*Expression:\h*(?<expression>[^\n]+)\h*\n\h*Evaluated:\h*(?<evaluated>[^\n]+)\h*\n"
-    matches = eachmatch(r, output)
-    for m in matches
-        msg = "Test Failed"
-        if !isnothing(m[:expression]) && !isnothing(m[:evaluated])
-                msg = string(msg, "\nExpression: ", m[:expression], "\n", "Evaluated: ", m[:evaluated])
-        end
-        if m[:path] === nothing
-            path = nothing
-            line = nothing
+function parse_file_line(failed_line)
+    r = r"(\e\[91m\e\[1m)?Test Failed(\e\[22m\e\[39m)? at (\e\[39m\e\[1m)?(?<path>[^\s\e]+)(\e\[22m)?"
+    m = match(r, failed_line)
+    m === nothing && return (nothing, nothing)
+
+    if m[:path] === nothing
+        return (nothing, nothing)
+    else
+        path_split_results = rsplit(m[:path], ":", limit=2)
+        if length(path_split_results) == 1
+            return (m[:path], nothing)
         else
-            path_split_results = rsplit(m[:path], ":", limit=2)
-            if length(path_split_results) == 1
-                path = m[:path]
-            else
-                path, line = path_split_results
-            end
-        end
-        with_logger(GitHubActionsLogger()) do
-            @error msg _file = path _line = line
+            path, line = path_split_results
+
+            # Try to make sure line number is parseable to avoid FPs
+            line = tryparse(Int, line) === nothing ? nothing : line
+            return (path, line)
         end
     end
+    return (nothing, nothing)
+end
+
+function readlines_until_print(stream, delim; keep_lines=true)
+    lines = String[]
+    while true
+        line = readline(stream; keep=true)
+        print(line)
+
+        # with `keep=true`, this should only happen when we're done?
+        # I think so...
+        if line == ""
+            return line, lines
+        end
+        if contains(line, delim)
+            return line, lines
+        else
+            keep_lines && push!(lines, line)
+        end
+    end
+
 end
 
 function test(args...; kwargs...)
     stream = Base.BufferStream()
     t = @async begin
         while !eof(stream)
-            line = readline(stream)
-            println(line)
-            if contains(line, "Test Failed")
-                failure_lines = [line]
-                while !contains(line, "Stacktrace:")
-                    line = readline(stream)
-                    println(line)
-                    push!(failure_lines, line)
-                end
-                parse_and_relog(join(failure_lines, "\n"))
+            # Iterate through and print until we get to "Test Failed"
+            failed_line, _ = readlines_until_print(stream, "Test Failed"; keep_lines=false)
+
+            # Try to parse file and line out
+            file, line = parse_file_line(failed_line)
+
+            # Couldn't parse? Probably a false positive, keep going
+            (isnothing(file) || isnothing(line)) && continue
+
+            # Could parse? Ok, grab everything until the stacktrace
+            _, msg_lines = readlines_until_print(stream, "Stacktrace:")
+
+            msg = chomp(join(msg_lines))
+
+            # Now log it out
+            with_logger(GitHubActionsLogger()) do
+                @error msg _file = file _line = line
             end
         end
     end
     Base.errormonitor(t)
-    return Pkg.test(args...; kwargs..., io=stream)
+    return try
+        Pkg.test(args...; kwargs..., io=stream)
+    finally
+        close(stream)
+    end
 end
 
 end # module
