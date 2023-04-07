98a8ba7e24f881578dd6151a22765e1d751c9be3
Sascha Mann <git@mail.saschamann.eu>
2023-04-07 12:19:20 +0200

    Add missing version check to test_harness
    #79 introduced a version requirement for annotations for installing the dependencies but not for the code that adds the annotations, resulting in a dependency error for certain versions.
    
    Fixes #80

--- test_harness.jl
+++ test_harness.jl
@@ -4,7 +4,7 @@ kwargs = Kwargs.kwargs(; coverage=ENV["COVERAGE"],
                          force_latest_compatible_version=ENV["FORCE_LATEST_COMPATIBLE_VERSION"],
                          julia_args=[string("--check-bounds=", ENV["CHECK_BOUNDS"])])
 
-if parse(Bool, ENV["ANNOTATE"]) && VERSION > v"1.8pre"
+if parse(Bool, ENV["ANNOTATE"]) && v"1.8pre" < VERSION < v"1.9.0-beta3"
     push!(LOAD_PATH, "@tests-logger-env") # access dependencies
     using GitHubActions, Logging
     global_logger(GitHubActionsLogger())
