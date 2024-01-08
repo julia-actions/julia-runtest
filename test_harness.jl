2b24f9b1dcb3920860914db01b79b956298b2329
David Little <david.frank.little@gmail.com>
2024-01-08 15:09:13 -0500

    enable julia arg `--compiled-modules` during testing (#91)
    * enable julia arg `--compiled-modules` during testing
    
    * fix typo
    
    * update `compiled_modules` docs
    
    * update `compiled_modules` docs with link to manual
    
    ---------
    
    Co-authored-by: Ian Butterworth <i.r.butterworth@gmail.com>

--- test_harness.jl
+++ test_harness.jl
@@ -2,7 +2,8 @@ import Pkg
 include("kwargs.jl")
 kwargs = Kwargs.kwargs(; coverage=ENV["COVERAGE"],
                          force_latest_compatible_version=ENV["FORCE_LATEST_COMPATIBLE_VERSION"],
-                         julia_args=[string("--check-bounds=", ENV["CHECK_BOUNDS"])])
+                         julia_args=[string("--check-bounds=", ENV["CHECK_BOUNDS"]),
+                                     string("--compiled-modules=", ENV["COMPILED_MODULES"])])
 
 if parse(Bool, ENV["ANNOTATE"]) && v"1.8pre" < VERSION < v"1.9.0-beta3"
     push!(LOAD_PATH, "@tests-logger-env") # access dependencies
