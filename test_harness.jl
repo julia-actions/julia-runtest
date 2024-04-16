7e64b540de5a30edb6820faf67c17a81efc40eda
Curtis Vogt <curtis.vogt@gmail.com>
2024-04-16 11:23:23 -0500

    Support specifying `allow_reresolve` (#107)

--- test_harness.jl
+++ test_harness.jl
@@ -2,6 +2,7 @@ import Pkg
 include("kwargs.jl")
 kwargs = Kwargs.kwargs(; coverage=ENV["COVERAGE"],
                          force_latest_compatible_version=ENV["FORCE_LATEST_COMPATIBLE_VERSION"],
+                         allow_reresolve=ENV["ALLOW_RERESOLVE"],
                          julia_args=[string("--check-bounds=", ENV["CHECK_BOUNDS"]),
                                      string("--compiled-modules=", ENV["COMPILED_MODULES"])])
 
@@ -11,7 +12,7 @@ if parse(Bool, ENV["ANNOTATE"]) && v"1.8pre" < VERSION < v"1.9.0-beta3"
     global_logger(GitHubActionsLogger())
     include("test_logger.jl")
     pop!(LOAD_PATH)
-    TestLogger.test(; kwargs...)        
+    TestLogger.test(; kwargs...)
 else
-    Pkg.test(; kwargs...)        
+    Pkg.test(; kwargs...)
 end
